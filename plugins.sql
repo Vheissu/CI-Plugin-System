904b0b3a3b4b69021963b63a5e39dbc82499e1f7
Dwayne Charrington <dwaynecharrington@gmail.com>
2011-03-30 07:53:57 +1000

    Brand new SQL file for the new revised database structure. The only thing that has changed is the column 'plugin_status' was removed as only activated plugins are now sotred in the database and not all plugins.

--- plugins.sql
+++ plugins.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50141
 File Encoding         : 65001
 
-Date: 2011-03-21 07:22:45
+Date: 2011-03-30 07:52:55
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -28,13 +28,12 @@ CREATE TABLE `plugins` (
   `plugin_author` varchar(120) DEFAULT NULL,
   `plugin_author_uri` varchar(120) DEFAULT NULL,
   `plugin_data` longtext,
-  `plugin_status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`plugin_id`),
-  UNIQUE KEY `Unique` (`plugin_system_name`)
+  KEY `plugin_index` (`plugin_system_name`)
 ) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of plugins
 -- ----------------------------
-INSERT INTO plugins VALUES ('1', 'helloworld', 'Hello World', 'http://ilikekillnerds.com', '1.0', 'A simple hello world plugin', 'Dwayne Charrington', 'http://ilikekillnerds.com', null, '0');
-INSERT INTO plugins VALUES ('2', 'cimarkdown', 'CI Markdown', 'http://ilikekillnerds.com', '1.0', 'Parses text for Markdown', 'Dwayne Charrington', 'http://ilikekillnerds.com', null, '1');
+INSERT INTO plugins VALUES ('1', 'cimarkdown', 'CI Markdown', 'http://ilikekillnerds.com', '1.0', 'Parses text for Markdown', 'Dwayne Charrington', 'http://ilikekillnerds.com', null);
+INSERT INTO plugins VALUES ('2', 'cimarkdown', 'CI Markdown', 'http://ilikekillnerds.com', '1.0', 'Parses text for Markdown', 'Dwayne Charrington', 'http://ilikekillnerds.com', null);
